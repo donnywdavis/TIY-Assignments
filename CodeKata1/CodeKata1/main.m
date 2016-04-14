5ff483e315e0e12bf01ce8b27b90788a57524fbb
Donny Davis <donnywdavis@icloud.com>
2016-04-14 13:20:11 -0400

    Set up project

---
+++ CodeKata1/CodeKata1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CodeKata1
+//
+//  Created by Donny Davis on 4/14/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
