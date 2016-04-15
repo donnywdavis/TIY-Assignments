132d064eb6bf6904e2a90f91dbb3cceddaaaa905
Donny Davis <donnywdavis@icloud.com>
2016-04-15 16:04:53 -0400

    Finish code kata project
    1) Create a function to return the sum of the multiples of 5 between 1
    and 1000.
    
    2) Create a function to return the largest palindrome of two 3-digit
    numbers from 100 to 1000.

--- CodeKata1/CodeKata1/main.m
+++ CodeKata1/CodeKata1/main.m
@@ -8,10 +8,74 @@
 
 #import <Foundation/Foundation.h>
 
+void multiplesOf3And5();
+void largestPalindrome();
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // Get the sum of the multiples of 3 and 5
+        multiplesOf3And5();
+        
+        // Get the largest palindrome product from two 3-digit numbers
+        largestPalindrome();
+        
     }
     return 0;
+}
+
+void multiplesOf3And5() {
+    
+    int range = 1000;
+    int sum = 0;
+    
+    for (int number = 0; number < range; number++) {
+        // Check if the number is a multiple of 3
+        if (number % 3 == 0) {
+            sum += number;
+            // Check if the number is a multiple of 5
+        } else if (number % 5 == 0) {
+            sum += number;
+        }
+    }
+    
+    NSLog(@"Sum of multiples of 3 and 5 below 1000: %d", sum);
+    
+}
+
+void largestPalindrome() {
+    
+    int result = 0, stringLength = 0;
+    NSString *resultAsString = @"";
+    NSMutableString *reversedString = [[NSMutableString alloc] init];
+    NSMutableString *theLargestPalindrome = [[NSMutableString alloc] init];
+
+    for (int sequence1 = 100; sequence1 < 1000; sequence1++) {
+        for (int sequence2 = 100; sequence2 < 1000; sequence2++) {
+            
+            [reversedString setString:@""];
+            
+            // Get our product
+            result = sequence1 * sequence2;
+            
+            // Convert our number to a string to monkey with it
+            resultAsString = [NSString stringWithFormat:@"%d", result];
+            stringLength = (int)[resultAsString length];
+            
+            // Loop through our string to reverse it
+            while (stringLength > 0) {
+                stringLength--;
+                [reversedString appendString:[NSString stringWithFormat:@"%c", (char)[resultAsString characterAtIndex:stringLength]]];
+            }
+            
+            // Compare the original result to the reverse to see if it's a palindrome
+            if ([resultAsString isEqualToString:reversedString]) {
+                [theLargestPalindrome setString:resultAsString];
+            }
+            
+        }
+    }
+    
+    NSLog(@"The largest palindrome is: %@", theLargestPalindrome);
+    
 }
